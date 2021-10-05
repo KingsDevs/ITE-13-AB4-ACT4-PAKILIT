fbbcbc36da7feec7ed22628156736534a5417f84
KingsDevs <sunnyckair@gmail.com>
2021-10-05 22:04:19 +0800

    exp

--- ITE-13-AB4-PAKILIT-ACT4.c
+++ ITE-13-AB4-PAKILIT-ACT4.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 
+
 int main()
 {
-    int experience, overtime, incentives;
+    int experience, overtime, incentive;
     printf("Enter your year of Experience: ");
     scanf("%d", &experience);
 
+    if(experience > 20)
+    {
+        incentive = experience * 4000;
+    }
+    else if(experience > 10)
+    {
+        incentive = experience * 3000;
+    }
+    else if (experience >= 5)
+    {
+        incentive = experience * 2000;
+    }
+    else
+    {
+
+    }
+    //printf("%d", incentive);
     
 
 }
